ddab7237f3fb9aab2372ab61cb9dbb6c04149c2a
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-09-11 10:34:52 -0300

    Make RequestSpecHelper only for request test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,7 +57,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   # JsonBody
-  config.include RequestSpecHelper
+  config.include RequestSpecHelper, type: :request
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
