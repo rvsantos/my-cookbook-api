1fc0c5d955c510fc8a13389ffa01f6f781f0d23f
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-09-11 01:17:10 -0300

    Adding devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'api_version_constraint'
 
 Rails.application.routes.draw do
 
+  devise_for :users
+
   scope module: 'api', default: { format: :json }, constraints: { subdomain: 'api' } do
     scope module: 'v1', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :recipes
